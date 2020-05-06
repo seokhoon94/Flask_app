07f5ff692f0bce8caadf258ac4c0df72d047f600
seokhoon94 <64902645+seokhoon94@users.noreply.github.com>
2020-05-06 21:58:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ from firebase_admin import db
 
 db_url = 'https://firstproject-afb38.firebaseio.com/'
 
-# 서비스 계정의 비공개 키 파일이름
 cred = credentials.Certificate("C:\\Users\\hsh96\\Desktop\\firstproject\\public\\firstproject-afb38-firebase-adminsdk-r6fpm-9c4723acd5.json")
 
 default_app = firebase_admin.initialize_app(cred, {'databaseURL':db_url})
 
-## 읽기
 ref = db.reference('Users')
 row = ref.get()
 
