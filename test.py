53d97eaf4d56978bad6f87e65c3f7a8944fab578
seokhoon94 <64902645+seokhoon94@users.noreply.github.com>
2020-05-07 20:17:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,11 @@ from flask import Flask
 app = Flask (__name__)
  
 @app.route('/')
-def main():   
-    return 'hello' 
+def main():
+    f = open("home\\ubuntu\\hi.txt", 'r')
+    data = f.read()
+    f.close() 
+    return data  
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
