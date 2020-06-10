dd1bdea4543ef76cfd4b269624854b70ff2db50c
seokhoon94 <64902645+seokhoon94@users.noreply.github.com>
2020-06-10 13:47:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from firebase_admin import db
 
 import json
 
-import pandas as pd
-from statsmodels.tsa.arima_model import ARIMA
+import time, datetime
 
 app = Flask (__name__)
 
@@ -47,73 +46,48 @@ class KnuSL():
 
 @app.route('/<target>')
 def test(target):
-   ref = db.reference(('DailyRecord/{}').format(target))
-   GroupN_date = ref.order_by_key().limit_to_last(1).get()
+    ref = db.reference(('DailyRecord/{}').format(target))
+    GroupN_date = ref.order_by_key().limit_to_last(4).get()
+    ksl = KnuSL
+    def calculator(record):
+        morphemes_of_record = twitter.morphs(record)
+        record_score = 0  # record 점수 계산
+        for j in range(0, len(morphemes_of_record)):
+            wordname = morphemes_of_record[j]
+            if ksl.data_list(wordname) != 'None':
+                record_score += int(ksl.data_list(wordname))
 
-   for key, val in GroupN_date.items():  # 참조한 돌봄일지의 record 저장
-      for key, val in val.items():
-         most_recent_diary = key
-         if key == 'record':
-            record = val
-            break
+        meal_score = 0  # meal 점수 계산
+        medicine_score = 0  # medicine 점수 계산
+        condition_score = 0
 
-   ksl = KnuSL
-   morphemes_of_record = twitter.morphs(record)
-   record_score = 0  # record 점수 계산
-   for j in range(0, len(morphemes_of_record)):
-      wordname = morphemes_of_record[j]
-      if ksl.data_list(wordname) != 'None':
-         record_score += int(ksl.data_list(wordname))
+        for key, val in GroupN_date.items():
+            for key, val in val.items():
+                for i in range(1, 4):
+                    if key == 'meal' + str(i):
+                        if val != "":
+                            meal_score += 1
+                    if key == 'medicine' + str(i) + 'check':
+                        if val != "false":
+                            medicine_score += 1
+                if key == 'condition':
+                    condition_score = val  # condition 점수 계산
 
-   meal_score = 0  # meal 점수 계산
-   medicine_score = 0  # medicine 점수 계산
-   condition_score = 0
+        total_score = record_score + meal_score + medicine_score + int(condition_score)
+        return total_score
 
-   for key, val in GroupN_date.items():
-      for key, val in val.items():
-         for i in range(1, 4):
-            if key == 'meal' + str(i):
-               if val != "":
-                  meal_score += 1
-            if key == 'medicine' + str(i) + 'check':
-               if val != "false":
-                  medicine_score += 1
-         if key == 'condition':
-            condition_score = val  # condition 점수 계산
+    count=0
+    for key, val in GroupN_date.items():  # 참조한 돌봄일지의 record 저장
+        date = key
+        for key, val in val.items():
+            most_recent_diary = key
+            if key == 'record':
+                record = val
 
-   total_score = record_score + meal_score + medicine_score + int(condition_score)
-
-
-   ref = db.reference(('Group/{}').format(target))  # 위험도 업데이트
-   ref.update({'risk': total_score})
-
-   return str(total_score)
-
-
-
-@app.route('/predict/<target>')
-def hello_world(target):
-  ref = db.reference(('WaterValue/{}').format(target))
-  js = ref.order_by_key().limit_to_last(365).get()
-
-  lst = []
-  lst2 = []
-
-  for data in js:
-      lst.append(data)
-      lst2.append(js[data])
-
-  series = pd.Series(lst2, index=lst)
-
-  diff_1 = series.diff(periods=1).iloc[1:]
-
-  model = ARIMA(series, order=(0, 1, 1))
-  model_fit = model.fit(trend='nc', full_output=True, disp=1)
-
-  fore = model_fit.forecast(steps=1)
-
-  return str(fore[0])
-
+                count=count+1
+                ref = db.reference(('Group/{}').format(target))  # 위험도 업데이트
+                ref.child('risk'+str(count)).update({'date':date[4:8],
+                                                      'value' : calculator(record)})
 
 
 @app.route('/servercheck')
